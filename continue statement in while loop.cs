6731fe3517b6bd1fce29227d357b493e49dac345
GauravPatil194 <98999566+GauravPatil194@users.noreply.github.com>
2022-04-30 12:46:40 +0530

    Create continue statement in while loop.cs

---
+++ continue statement in while loop.cs
@@ -0,0 +1,21 @@
+// demonstration of continue statement in while loop.
+Using System;
+namespace CSharp_Shell
+{
+    Public static class Program 
+    {
+        Public static void Main() 
+        {
+           Int i=1;
+           While(i<=12)
+           {
+             	I++;
+           If(i==5)
+           {
+           	Continue;
+           }
+            Console.WriteLine(i);
+           }
+        }
+    }  
+    }
