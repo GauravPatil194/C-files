722f3d10cbc2a999e2553c88d8f7a6d3942a6609
GauravPatil194 <98999566+GauravPatil194@users.noreply.github.com>
2023-07-28 13:13:48 +0530

    Create Add.c

---
+++ Add.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+  int num1,num2;
+  int sum=0;
+  printf("Enter a num : ");
+  scanf("%d",&num1);
+  printf("Enter a num : ");
+  scanf("%d",&num2);
+
+  sum = num1 +num2;
+ printf("sum =%d",sum);
+}
